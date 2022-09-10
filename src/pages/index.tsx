285c7f40fdfd762c3cdea0a6b4af995bc2c07dfa
andresdosantos <andres.dosantosbritoamaral@gmail.com>
2022-09-10 15:01:31 -0300

    feat(src/styles): add stitches

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+})
+
+export default function Home() {
+  return <h1>Hello Next.JS</h1>
+}
