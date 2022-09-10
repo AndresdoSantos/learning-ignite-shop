e2d752fc2370a1be8fc7acc657d955fafcbe9515
andresdosantos <andres.dosantosbritoamaral@gmail.com>
2022-09-10 17:32:56 -0300

    feat(src/assets): add carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,59 @@
-import { styled } from '../styles'
+import Image from 'next/future/image'
+import { useKeenSlider } from 'keen-slider/react'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-})
+import { HomeContainer, HomeProduct } from '../styles/pages/home'
+
+import shirt1 from '../assets/1.svg'
+import shirt2 from '../assets/2.svg'
+import shirt3 from '../assets/3.svg'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
-  return <h1>Hello Next.JS</h1>
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <HomeProduct className="keen-slider__slide">
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </HomeProduct>
+
+      <HomeProduct className="keen-slider__slide">
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </HomeProduct>
+
+      <HomeProduct className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </HomeProduct>
+
+      <HomeProduct className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </HomeProduct>
+    </HomeContainer>
+  )
 }
